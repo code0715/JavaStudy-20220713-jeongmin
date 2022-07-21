49d0a25a3dc712574995732b63fe5d276246174b
code0715 <jungmin7318@gmail.com>
2022-07-21 12:47:29 +0900

    챕터 7 반복

---
+++ src/j07_반복/Continue2.java
@@ -0,0 +1,24 @@
+package j07_반복;
+
+public class Continue2 {
+
+	public static void main(String[] args) {
+		
+		String address = "부산 동래구 사직동 중앙대로";
+		String tempAddress = null;
+				
+		for(int i = 0; i < address.length(); i++) {
+			String subaaddress = address.substring(i, i+ 1);
+			if(subaaddress.equals(" ")) {
+				continue;
+			}
+			if(i == 0) {
+				tempAddress = subaaddress;
+			}else {
+				tempAddress += subaaddress;
+			}
+		}
+		System.out.println(tempAddress);
+	}
+
+}
