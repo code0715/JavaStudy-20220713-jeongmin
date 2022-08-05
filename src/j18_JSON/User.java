f24f6bef75ff3f9b5141a1b854049c4604d22176
code0715 <jungmin7318@gmail.com>
2022-08-05 12:48:38 +0900

    챕터 18 JSON

---
+++ src/j18_JSON/User.java
@@ -0,0 +1,35 @@
+package j18_JSON;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class User {
+	/*
+	 * serialize = 객체를 json으로 변환할때
+	 * deserialize = json을 객체로 변환할때
+	 */
+	@Expose(serialize = true, deserialize = true)
+	private int userCode;
+	@Expose(serialize = true, deserialize = true)
+	@SerializedName("userId")
+	private String username;
+	@Expose(serialize = false, deserialize = true)
+	private transient String password;
+	@Expose(serialize = true, deserialize = true)
+	private String name;
+	@Expose(serialize = true, deserialize = false)
+	private String email;
+	@Expose(serialize = true, deserialize = true)
+	private String address;
+	@Expose(serialize = true, deserialize = true)
+	private String phone;
+	@Expose(serialize = true, deserialize = true)
+	private List<String> hobby;
+}
